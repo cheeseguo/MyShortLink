5c0b6a73c4e5ba4f3ac1130c1b4222ce9dc536f4
justin <justin.yunfeng@gmail.com>
2025-12-10 12:20:51 +0800

    feature: 开发短链接后管系统-查询用户信息脱敏功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -9,4 +9,6 @@ import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
  */
 public interface UserService extends IService<UserDO> {
     UserRespDTO getUserByUsername(String username);
+
+    Boolean hasUserName(String username);
 }
